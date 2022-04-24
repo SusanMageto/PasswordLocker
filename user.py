c4f97ab6e4cea238f0ccc0487f16aae3dcc939ac
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-24 12:09:15 +0300

    creates an empty list of users and defines the attributes therein

--- user.py
+++ user.py
@@ -2,3 +2,16 @@ class User:
   """
   Class that generates new instances of user accounts
   """
+  
+  user_list=[] # Empty list of users
+  
+  def __init__(self, user_name, password):
+    '''
+    init method that helps us define properties for our object
+    Args:
+    user_name:name of the user
+    user_password:password of the user
+    '''
+    self.user_name= user_name
+    self.user_password= password 
+  
