f790be4f49aa1f2b9b910526d3feb683ac420ff8
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-24 14:44:10 +0300

    creates a save_user function then a save_user method to save the user

--- run.py
+++ run.py
@@ -7,7 +7,17 @@ from user import User
 def create_user(name,password):
   '''
   function to create new user
+  Args:
+  name:the name user inputs
+  password:the password the user inputs
   '''
   new_user=User(name,password)
   return new_user
   
+def save_user(user):
+  '''
+  function that saves the user account
+  Args:
+  user:the user account to be saved
+  '''
+  user.save_user()
