f98b39a9fd8f2ed5445744dca56247916f7fe503
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-24 14:24:23 +0300

    creates a new function that allows us to create a new_user

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3.9
+'''
+This is the file that will run the application
+'''
 from user import User
+
+def create_user(name,password):
+  '''
+  function to create new user
+  '''
+  new_user=User(name,password)
+  return new_user
+  
