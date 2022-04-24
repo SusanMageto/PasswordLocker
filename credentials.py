f1f3862c631f89ca00dc5712cce4008c0e4b04f0
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-24 15:12:17 +0300

    creates a credentials class

---
+++ credentials.py
@@ -0,0 +1,15 @@
+'''
+credential module by Sue
+Imports the random module from python for password generation
+imports the string module from python for password generation
+imports the user from the user module so as to get access to the user
+'''
+from random import choice
+import string
+from user import User
+
+class credentials:
+  
+ """
+Class that generates new instances of credentials accounts
+"""
