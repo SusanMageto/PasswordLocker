788d1fb7dc1a609f3a912205de86b31df19207b4
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-24 15:30:45 +0300

    creates a method that saves credentials to the credentials list

--- credentials.py
+++ credentials.py
@@ -8,8 +8,28 @@ from random import choice
 import string
 from user import User
 
-class credentials:
+class Credentials:
   
  """
-Class that generates new instances of credentials accounts
+Class that generates new instances of credentials for the users
 """
+
+credentials_list=[] #Empty list of credentials
+
+def __init__(self,password,credential_name,credential_password):
+  '''
+  init method that helps us define properties for our user object
+  Args:
+  password:the user's password
+  credential_name:name of the credential account
+  credential password:password for the credential account
+  '''
+  self.password=password
+  self.credential_name=credential_name
+  self.credential_password=credential_password
+  
+def save_credentials(self):
+  '''
+  a method that saves credentials to credentials list
+  '''
+  Credentials.credentials_list.append(self)
