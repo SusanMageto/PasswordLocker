ef7d1703402abf46fee033dc8808dc18393e4228
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-25 06:18:28 +0300

    creates class methods to return the list of users and check if a user exists in the list

--- user.py
+++ user.py
@@ -1,3 +1,7 @@
+from credentials import Credentials
+from run import display_users
+
+
 class User:
   """
   Class that generates new instances of user accounts
@@ -21,4 +25,52 @@ class User:
     '''
     User.user_list.append(self)
     
+@classmethod
+def find_credentials(cls,name):
+  '''
+  a class method used to check if there is correct importation of the credentials
+  Args:
+  name:credential name
+  '''
+  for credential in Credentials.credential_list:
+    if credential.credential_name==name:
+      return True
+    return False
+@classmethod
+def sign_in(cls, name, password):
+  '''
+  a class method that allows the user to sign in
+  Args:
+  name:username
+  password:userpassword
+  '''
+  # Search for the user in the user list
+  for user in cls.user_list:
+    if user.username == name and user.userpassword == password:
+      return Credentials.credential_list
+    return False
+  
+@classmethod 
+def display_users(cls):
+  '''
+  a class method that returns the list of users
+  '''
+  return cls.user_list
+
+@classmethod
+def user_exist(cls,name):
+  '''
+  method to check if a user exists in the list of users.
+  '''
+  for user in cls.user_list:
+    if user.username == name:
+      return True
+    return False
+  
+            
+                
+            
+       
+
+  
   
