72b486e69eb9f6e63f0dd0f84ae4ab4a397bf6f2
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-26 06:11:30 +0300

    revise the credentials test files

---
+++ credentials-test.py
@@ -0,0 +1,73 @@
+import unittest
+from credentials import Credential
+
+
+class TestCredential(unittest.TestCase):
+    """
+    Test class that defines test cases for the Credential contact class behaviours
+    """
+
+    def setUp(self) -> None:
+        """
+        Set up method to be run before each test cases
+        :return:
+        """
+        self.new_credentials = Credential("twitter", "paul", "tweet32323")
+
+    def test_save_credential(self):
+        """
+        test_save_credential test case tests if a credential is saved
+        correctly in the credential list
+        :return:
+        """
+        self.new_credentials.save_credential()
+
+        self.assertEqual(len(Credential.credential_list), 1)
+
+    def tearDown(self) -> None:
+        """
+        tearDown method that does clean up after each test case has run.
+        :return:
+        """
+        Credential.credential_list = []
+
+    def test_save_multiple_credentials(self):
+        """
+                test_save_multiple_credentials test case tests if multiple credentials can be saved correctly
+                in the credential list
+                :return:
+                """
+        self.new_credentials.save_credential()
+        new_user = Credential("instagram", "susan", "insta67836")
+        new_user.save_credential()
+
+        self.assertEqual(len(Credential.credential_list), 2)
+
+    def test_delete_credential(self):
+        """
+                test_delete_credential case tests if a credential can be deleted successfully from the
+                credential list
+                :return:
+                """
+        self.new_credentials.save_credential()
+        new_user = Credential("instagram", "susan", "insta67836")
+        new_user.save_credential()
+
+        new_user.delete_credential()
+
+        self.assertEqual(len(Credential.credential_list), 1)
+
+    def test_display_user_credentials(self):
+        """
+        test_display_user_credentials tests if we can display the users details correctly
+        :return:
+        """
+        self.new_credentials.save_credential()
+        new_user = Credential("instagram", "susan", "insta67836")
+        new_user.save_credential()
+
+        self.assertEqual(Credential.display_credentials(), Credential.credential_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
