08d90d771cc9d20f3b2882321dcef158e0a3ffcc
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-26 06:27:48 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,22 @@
+# PASSWORD LOCKER
+#### 
+# Author: **Susan Mageto**
+## Description
+This application will help users manage their passwords and even generate new passwords.The site also takes in users social media accounts and saves them safely.
+## User Stories
+* As a user, I want to create a password locker account with my details, a login username and password.
+* As a user, I want to store my already existing account credentials in the application. Assuming I already have a twitter account, I want to store my already existing twitter username and password in the application.
+* As a user, I want to create new account credentials in the application. For example, if I have not yet signed up for Instagram, I would want to create a credentials account for Instagram in the application and the application generates a password for me to use when I sign up for Instagram.
+* As a user, I want to have the option of putting in a password that I want to use for the new credential account. For example, when creating my Instagram credential account, I want to have an option of putting in the password I want to use instead of having the application generate a password for me.
+* As a user, I also want to view my various account credentials and their passwords in the application.
+* As a user, I want to delete a credentials account that I no longer need in the application.
+## Setup/Installation Requirements
+Run the main.py file
+## Known Bugs
+There are currently no bugs. If you experience any bugs, don't hesitate to contact me.
+## Technologies Used
+The site has been developed using Python
+## Support and contact details
+Contact: susanmageto.mageto@gmail.com
+### License
+[MIT](https://github.com/SusanMageto)
--- credentials.py
+++ credentials.py
@@ -1,49 +1,79 @@
+# from cryptography.fernet import Fernet
+# import re
+# import ctypes
+# import time
+# import os
+# import sys
+
+
+# class Credentials():
+    
+#     credentials_list = []
+
+#     user_credentials_list = []
 import random
+from random import choice
 import string
-
-
-class Credential:
-    credential_list = []  # Created an empty credential list
-
-    def __init__(self, social_media, account, password):
-        """
-        The init method defines the properties of the object
-        :param social_media:
-        :param account:
-        :param password:
-        """
-        self.social = social_media
-        self.account = account
+from user import User
+class Credentials:
+  credentials_list=[] #Empty list of credentials
+  def __init__(self, username, account_name, password):
+        self.username = username
+        self.account_name = account_name
         self.password = password
 
-    def save_credential(self):
-        """
-        Saving the user credentials
-        :return:
-        """
-        Credential.credential_list.append(self)
+def save_credentials(self):
+        Credentials.credentials_list.append(self)
 
-    def delete_credential(self):
-        """
-        Deleting the user credentials
-        :return:
-        """
-        Credential.credential_list.remove(self)
+def delete_credentials(self):
+        Credentials.credentials_list.remove(self)
 
-    @classmethod
-    def display_credentials(cls):
-        """
-        Function to display credentials
-        :return:
-        """
-        return cls.credential_list
 
-    def automatic_generated_password(length=12, password=string.digits + string.ascii_letters + string.ascii_uppercase):
-        """
-        Function to generate a random password
-        :param password:
-        :return:
-        """
 
-        random_password = ''.join(random.choice(password) for i in range(length))
-        return random_password
+@classmethod
+def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
+
+@classmethod
+def find_credentials(cls, account):
+        """
+        Method that takes in a account_name and returns a credential that matches that account_name.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
+@classmethod
+def copy_password(cls,account):
+        found_credentials = Credentials.find_credential(account)
+        pyperclip.copy(found_credentials.password)
+
+@classmethod
+def if_credentials_exist(cls, account):
+        """
+        Method that checks if a credential exists from the credential list and returns true or false depending if the credential exists.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
+@classmethod
+def display_credentials(cls):
+        """
+        Method that returns all items in the credentials list
+        """
+        return cls.credentials_list
+
+def generatePassword(stringLength=8):
+        """Generate a random password string of letters and digits and special characters"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
+    
+
+   
